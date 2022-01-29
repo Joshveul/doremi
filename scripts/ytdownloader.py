cfecc7ffd45680e062c85b6faf71324ed84342b0
Irvin <joshluevano@informaticos.com>
2022-01-29 15:33:32 +0100

    Added image download to ytdowloader script

--- scripts/ytdownloader.py
+++ scripts/ytdownloader.py
@@ -1,8 +1,9 @@
 from pytube import YouTube
 import sys
 import pymongo
-from datetime import date
 from datetime import datetime
+import requests
+import shutil
 
 # Instanciate the database
 myclient = pymongo.MongoClient("mongodb://localhost:37017/")
@@ -24,8 +25,10 @@ lastPlayed = datetime.now
 
 # Set some constants
 videoDocQuery = { "id": videoId }
+staticFolderPath = './static/'
+downloadPath = 'archive'
 fileName = videoId + '.mp4'
-downloadPath = 'static/archive'
+thumbnailPath = downloadPath  + '/' + videoId + '.jpg'
 
 # If the video record exists in the database, terminate the process here
 print('Looking video in database...', videoDocQuery)
@@ -35,8 +38,9 @@ if (videoDoc):
     print('Video already exists')
     exit()
 
+
 # Store the record in the database to prevent duplicate requests for the same video
-mydict = { "videoId": videoId, "title": title, "artist": artist, "thumbnail": thumbnail, "channel": channel, "duration": duration, "firstAddedBy": firstAddedBy, "addedBy": addedBy, "timesAdded": timesAdded, "timesPlayed": timesPlayed, "timesRemoved": timesRemoved, 'downloading': True, 'fileLocation': downloadPath + '/' + fileName}
+mydict = { "videoId": videoId, "title": title, "artist": artist, "thumbnail": thumbnailPath, "channel": channel, "duration": duration, "firstAddedBy": firstAddedBy, "addedBy": addedBy, "timesAdded": timesAdded, "timesPlayed": timesPlayed, "timesRemoved": timesRemoved, 'downloading': True, 'fileLocation': downloadPath + '/' + fileName}
 mycol.insert_one(mydict)
 print('Record stored: ', mydict)
 
@@ -46,9 +50,24 @@ yt = YouTube('http://youtube.com/watch?v=' + videoId)
 hqStream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 
 # Download video to static archive folder
-print('Downloading...')
-hqStream.download('./' + downloadPath, fileName)
-print('Download complete')
+print('Downloading video...')
+hqStream.download(staticFolderPath + downloadPath, fileName)
+print('Video Successfully Downloaded: ./' + downloadPath + fileName)
+
+# Download thumbnail to static archive folder
+
+r = requests.get(thumbnail, stream = True)
+if r.status_code == 200:
+    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+    r.raw.decode_content = True
+    
+    # Open a local file with wb ( write binary ) permission.
+    with open(staticFolderPath + thumbnailPath, 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+        
+    print('Image sucessfully Downloaded: ' + './' + thumbnailPath)
+else:
+    print('Image Couldn\'t be retreived')
 
 # Exit
 print('done')
