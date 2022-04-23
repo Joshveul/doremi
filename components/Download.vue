5186deb651e9fe67741beab239efd1a2d0b65630
Irvin <joshluevano@informaticos.com>
2022-04-23 09:11:30 +0200

    WIP

---
+++ components/Download.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-btn
+    icon
+    class="ml-auto mt-2 mr-2"
+    @click="validateAndAdd()"
+  >
+    <v-icon>mdi-download</v-icon>
+  </v-btn>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { downloadVideo } from '~/modules/utils'
+export default {
+  props: {
+    songId: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    ...mapState(['userData'])
+  },
+  methods: {
+    validateAndAdd () {
+      this.addToFavorites(this.songId)
+    },
+    triggerDownload (songId) {
+
+    }
+  }
+}
+</script>
